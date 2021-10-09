6e34f81f5c3d289e249b91aaccc86fd0362e467a
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-09 16:12:07 +0700

    fix: footer

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section-small home__content">
+    <div class="uk-container uk-padding-remove">
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-header">
 
+            </div>
+            <div class="uk-card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
