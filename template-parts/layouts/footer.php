83d25dc709d4fc6bcb367c886a17cc842c120d2e
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-09 10:13:11 +0700

    init

---
+++ template-parts/layouts/footer.php
@@ -0,0 +1,29 @@
+<!--footer-->
+<div class="footer">
+    <!--top-->
+    <div class="footer__top uk-section-small">
+        <div>
+
+        </div>
+    </div>
+    <!--/top-->
+
+    <!--center-->
+    <div class="footer__center uk-section-small">
+
+    </div>
+    <!--/center-->
+
+    <!--bottom-->
+    <div class="footer__bottom uk-section-xsmall">
+        <div class="uk-container">
+            <div class="footer__bottom__copyRightTxt">© Bản quyền thuộc về <span>Evo Themes</span> | Cung cấp bởi <a href="">Sapo</a></div>
+        </div>
+    </div>
+    <!--/bottom-->
+</div>
+<!--/footer-->
+</div>
+<!--/app-->
+</body>
+</html>
