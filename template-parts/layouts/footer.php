2ef2392673604992b9292218d9bd7bd4f6b86568
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-09 11:54:14 +0700

    add: footer

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -2,15 +2,143 @@
 <div class="footer">
     <!--top-->
     <div class="footer__top uk-section-small">
-        <div>
-
+        <div class="uk-container">
+            <div class="uk-grid-small uk-grid-80-m uk-flex-middle" uk-grid>
+                <div class="uk-width-2-5@m">
+                    <h3 class="uk-h3 uk-margin-remove footer__top__title">ĐĂNG KÝ NHẬN TIN</h3>
+                    <div class="footer__top__txt">Nhận thông tin sản phẩm mới nhất, tin khuyến mãi và nhiều hơn nữa.</div>
+                </div>
+                <div class="uk-width-expand">
+                    <div class="uk-grid-collapse" uk-grid>
+                        <div class="uk-width-expand">
+                            <input class="uk-input footer__top__input" type="text" placeholder="Địa chỉ Email">
+                        </div>
+                        <div class="uk-width-auto">
+                            <button type="submit" class="uk-button uk-button-secondary footer__top__btnSubmit">Đăng ký</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <!--/top-->
 
     <!--center-->
     <div class="footer__center uk-section-small">
-
+        <div class="uk-container">
+            <div class="uk-grid-small uk-grid-30-m uk-child-width-1-4@m" uk-grid>
+                <div>
+                    <h4 class="uk-h4 footer__center__title">Evo Việt Nam</h4>
+                    <ul class="uk-nav footer__center__nav">
+                        <li><a href="">Trang chủ</a></li>
+                        <li><a href="">Giới thiệu</a></li>
+                        <li><a href="">Sản phẩm</a></li>
+                        <li><a href="">Săn Deal</a></li>
+                        <li><a href="">Hàng Like New</a></li>
+                        <li><a href="">Tin tức</a></li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="uk-h4 footer__center__title">Dành cho khách hàng</h4>
+                    <ul class="uk-nav footer__center__nav">
+                        <li><a href="">Đồ điện tử và công nghệ</a></li>
+                        <li><a href="">Đồng hồ hàng hiệu</a></li>
+                        <li><a href="">Thời trang nữ</a></li>
+                        <li><a href="">Thời trang nam</a></li>
+                        <li><a href="">Mẹ, Bé và Đồ chơi</a></li>
+                        <li><a href="">Làm đẹp</a></li>
+                        <li><a href="">Thế giới nước hoa</a></li>
+                        <li><a href="">Đồ chơi và Games</a></li>
+                        <li><a href="">Phụ tùng, phụ kiện xe hơi</a></li>
+                        <li><a href="">Thể thao, du lịch và dã ngoại</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="uk-margin-small footer__center__margin20">
+                        <h4 class="uk-h4 footer__center__title">Liên hệ với chúng tôi</h4>
+                        <ul class="uk-list footer__center__addRess">
+                            <li><span>Địa chỉ:</span> 70 Lu Gia, Ward 15, District 11, Ho Chi Minh City</li>
+                            <li><span>Email:</span> support@sapo.vn</li>
+                            <li><span>Điện thoại:</span> 1900 6750</li>
+                            <li><span>Zalo:</span> 1900 6750</li>
+                        </ul>
+                    </div>
+                    <div class="uk-margin-small footer__center__margin20">
+                        <h4 class="uk-h4 footer__center__title">Đơn vị vận chuyển</h4>
+                        <div class="uk-child-width-1-2@m uk-child-width-auto uk-grid-5" uk-grid>
+                            <div>
+                                <a href=""><img src="images/fot_ship_1.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img src="images/fot_ship_2.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img src="images/fot_ship_3.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img src="images/fot_ship_4.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img src="images/fot_ship_5.png" alt=""></a>
+                            </div>
+                            <div>
+                                <a href=""><img src="images/fot_ship_6.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-margin-small footer__center__margin20">
+                        <h4 class="uk-h4 footer__center__title">Chấp nhận thanh toán</h4>
+                        <div class="uk-child-width-auto uk-grid-7" uk-grid>
+                            <div><a href=""><img src="images/payment_1.svg" alt=""></a></div>
+                            <div><a href=""><img src="images/payment_2.svg" alt=""></a></div>
+                            <div><a href=""><img src="images/payment_3.svg" alt=""></a></div>
+                            <div><a href=""><img src="images/payment_4.svg" alt=""></a></div>
+                            <div><a href=""><img src="images/payment_5.svg" alt=""></a></div>
+                            <div><a href=""><img src="images/payment_6.svg" alt=""></a></div>
+                        </div>
+                    </div>
+                    <div class="uk-margin-small footer__center__margin20">
+                        <div class="uk-child-width-auto uk-grid-7" uk-grid>
+                            <div class="fb">
+                                <a href="" target="_blank" class="footer__center__iconSocial">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25px" height="25px" viewBox="0 0 96.124 96.123" style="enable-background:new 0 0 96.124 96.123;" xml:space="preserve">
+                                        <path d="M72.089,0.02L59.624,0C45.62,0,36.57,9.285,36.57,23.656v10.907H24.037c-1.083,0-1.96,0.878-1.96,1.961v15.803   c0,1.083,0.878,1.96,1.96,1.96h12.533v39.876c0,1.083,0.877,1.96,1.96,1.96h16.352c1.083,0,1.96-0.878,1.96-1.96V54.287h14.654   c1.083,0,1.96-0.877,1.96-1.96l0.006-15.803c0-0.52-0.207-1.018-0.574-1.386c-0.367-0.368-0.867-0.575-1.387-0.575H56.842v-9.246   c0-4.444,1.059-6.7,6.848-6.7l8.397-0.003c1.082,0,1.959-0.878,1.959-1.96V1.98C74.046,0.899,73.17,0.022,72.089,0.02z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#EBE7E7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div class="tt">
+                                <a href="" target="_blank" class="footer__center__iconSocial">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="25px" height="25px">
+                                        <path d="M512,97.248c-19.04,8.352-39.328,13.888-60.48,16.576c21.76-12.992,38.368-33.408,46.176-58.016    c-20.288,12.096-42.688,20.64-66.56,25.408C411.872,60.704,384.416,48,354.464,48c-58.112,0-104.896,47.168-104.896,104.992    c0,8.32,0.704,16.32,2.432,23.936c-87.264-4.256-164.48-46.08-216.352-109.792c-9.056,15.712-14.368,33.696-14.368,53.056    c0,36.352,18.72,68.576,46.624,87.232c-16.864-0.32-33.408-5.216-47.424-12.928c0,0.32,0,0.736,0,1.152    c0,51.008,36.384,93.376,84.096,103.136c-8.544,2.336-17.856,3.456-27.52,3.456c-6.72,0-13.504-0.384-19.872-1.792    c13.6,41.568,52.192,72.128,98.08,73.12c-35.712,27.936-81.056,44.768-130.144,44.768c-8.608,0-16.864-0.384-25.12-1.44    C46.496,446.88,101.6,464,161.024,464c193.152,0,298.752-160,298.752-298.688c0-4.64-0.16-9.12-0.384-13.568    C480.224,136.96,497.728,118.496,512,97.248z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#EBE7E7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div class="yt">
+                                <a href="" target="_blank" class="footer__center__iconSocial">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                                        <path d="M490.24,113.92c-13.888-24.704-28.96-29.248-59.648-30.976C399.936,80.864,322.848,80,256.064,80
+                                                 c-66.912,0-144.032,0.864-174.656,2.912c-30.624,1.76-45.728,6.272-59.744,31.008C7.36,138.592,0,181.088,0,255.904
+                                                 C0,255.968,0,256,0,256c0,0.064,0,0.096,0,0.096v0.064c0,74.496,7.36,117.312,21.664,141.728
+                                                 c14.016,24.704,29.088,29.184,59.712,31.264C112.032,430.944,189.152,432,256.064,432c66.784,0,143.872-1.056,174.56-2.816
+                                                 c30.688-2.08,45.76-6.56,59.648-31.264C504.704,373.504,512,330.688,512,256.192c0,0,0-0.096,0-0.16c0,0,0-0.064,0-0.096
+                                                 C512,181.088,504.704,138.592,490.24,113.92z M192,352V160l160,96L192,352z"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div class="ins">
+                                <a href="" target="_blank" class="footer__center__iconSocial">
+                                    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m75 512h362c41.355469 0 75-33.644531 75-75v-362c0-41.355469-33.644531-75-75-75h-362c-41.355469 0-75 33.644531-75 75v362c0 41.355469 33.644531 75 75 75zm-45-437c0-24.8125 20.1875-45 45-45h362c24.8125 0 45 20.1875 45 45v362c0 24.8125-20.1875 45-45 45h-362c-24.8125 0-45-20.1875-45-45zm0 0"></path><path d="m256 391c74.4375 0 135-60.5625 135-135s-60.5625-135-135-135-135 60.5625-135 135 60.5625 135 135 135zm0-240c57.898438 0 105 47.101562 105 105s-47.101562 105-105 105-105-47.101562-105-105 47.101562-105 105-105zm0 0"></path><path d="m406 151c24.8125 0 45-20.1875 45-45s-20.1875-45-45-45-45 20.1875-45 45 20.1875 45 45 45zm0-60c8.269531 0 15 6.730469 15 15s-6.730469 15-15 15-15-6.730469-15-15 6.730469-15 15-15zm0 0"></path></svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!--/center-->
 
