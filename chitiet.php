1487cef813ca5fa55868346146f90f0509ce2f0f
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-12 03:33:00 +0700

    update chitiet

---
+++ chitiet.php
@@ -0,0 +1,175 @@
+<?php $data["title"] = "Chi tiet sp"; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="chitiet__content">
+    <div class="uk-section-xsmall chitiet__section1">
+        <div class="uk-container">
+            <ul class="uk-breadcrumb chitiet__breadcrumb">
+                <li><a href="" uk-icon="icon: home; ratio: 1"></a></li>
+                <li><a href="">Máy in - TB văn phòng</a></li>
+                <li><span>Máy in văn phòng</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="uk-container uk-padding-remove">
+        <div class="uk-card chitiet__card uk-card-default uk-padding-small chitiet__margin1">
+            <div class="uk-grid-small uk-grid-20-m" uk-grid>
+                <div class="uk-width-1-3@m">
+
+                </div>
+                <div class="uk-width-expand">
+                    <h1 class="uk-h1 chitiet__product__title">Máy in Laser Canon LBP 2900</h1>
+                    <div class="chitiet__product__boxMasp">
+                        <div class="uk-child-width-auto uk-grid-small uk-grid-20-m uk-flex-bottom" uk-grid>
+                            <div>
+                                <div class="chitiet__product__boxMasp__txt"><span>Mã SP:</span> MICN0012</div>
+                            </div>
+                            <div>
+                                <div class="uk-child-width-auto uk-grid-5 uk-flex-middle" uk-grid>
+                                    <div>
+                                        <div class="chitiet__product__boxMasp__rate uk-position-relative">
+                                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                                            <div class="uk-position-cover uk-overflow-hidden" style="width: 75%">
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span class="chitiet__product__boxMasp__txt">9 đánh giá</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="uk-list chitiet__product__listInfo">
+                        <li>Canon LBP 2900</li>
+                        <li>Loại máy in : Máy in laser đen trắng</li>
+                        <li>Xuất xứ: <span>China</span></li>
+                        <li>Khổ giấy tối đa : A4, B5, A5, LGL, LTR, Executive, Giấy in bì thư C5/COM10/DL, Monarch</li>
+                        <li class="item uk-hidden">Độ phân giải : 600 x 600dpi</li>
+                        <li class="item uk-hidden">Kết nối: USB 2.0. CAPT 2.1.</li>
+                        <li class="item uk-hidden">Tốc độ in đen trắng: 12 trang/phút</li>
+                        <li class="item uk-hidden">Tốc độ in màu: Không</li>
+                        <li class="item uk-hidden">Mực in: Cartridge 303 (2,000 pages)/Cartridge 303 Twin Pack</li>
+                        <li class="item uk-hidden">Khối lượng: 5.7kg</li>
+                    </ul>
+                    <div class="uk-margin-small-bottom"><a id="js-viewmore-summary" href="javascript:void(0)">Xem thêm ></a></div>
+                    <script>
+                        $("#js-viewmore-summary").click(function(){
+                            $('.chitiet__product__listInfo .item').toggleClass("uk-hidden");
+                            if($(this).text()=='Xem thêm >') $(this).text('< Thu gọn');
+                            else $(this).text('Xem thêm >');
+                        });
+                    </script>
+                    <div class="uk-padding-small chitiet__product__infoPriceContainer">
+                        <div class="uk-grid-10" uk-grid>
+                            <div class="uk-width-1-1">
+                                <div class="uk-child-width-auto uk-grid-5 uk-grid-15-m uk-flex-middle" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <div class="chitiet__product__infoPriceContainer__label">Giá niêm yết:</div>
+                                    </div>
+                                    <div class="uk-width-auto uk-width-expand@m">
+                                        <del class="chitiet__product__infoPriceContainer__priceOld">5.990.000 đ</del>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="uk-width-1-1">
+                                <div class="uk-child-width-auto uk-grid-5 uk-grid-15-m" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <div class="chitiet__product__infoPriceContainer__label">Giá khuyến mại:</div>
+                                    </div>
+                                    <div class="uk-width-auto uk-width-expand@m">
+                                        <div><span class="chitiet__product__infoPriceContainer__priceNew">3.990.000 đ</span> <span>[Giá đã có VAT]</span></div>
+                                        <div class="chitiet__product__infoPriceContainer__km">Giảm thêm tới 100.000đ ( Áp dụng đơn hàng từ 2 triệu) khi quẹt thẻ VNPAY -
+                                            <a href="">Xem chi tiết >>></a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chitiet__product__baohanh">Bảo hành: CHÍNH HÃNG 12 tháng tại CANON VIỆT NAM</div>
+                    <div class="chitiet__product__option">
+                        <div class="chitiet__product__option__label">Tuỳ chọn sản phẩm</div>
+                        <div class="uk-child-width-1-2 uk-grid-10 uk-grid-match" uk-grid>
+                            <?php
+                            $data = array(
+                                array(
+                                    'txt' => 'Máy in HP Neverstop Laser 1000w 4RY23A',
+                                    'price' => '3.059.000 đ',
+                                ),
+                                array(
+                                    'txt' => 'Máy in Canon LBP161DN+',
+                                    'price' => '5.149.000 đ',
+                                ),
+                                array(
+                                    'txt' => 'Máy in Laser Canon LBP 2900',
+                                    'price' => '3.990.000 đ',
+                                ),
+                                array(
+                                    'txt' => 'Máy in Canon LBP 113W',
+                                    'price' => '3.749.000 đ',
+                                ),
+                            );
+                            foreach ($data as $k=>$v): ?>
+                            <div>
+                                <label>
+                                    <input class="uk-radio chitiet__product__option__check uk-hidden" type="radio" name="radio2" <?= ($k==0)? 'checked':'' ?>>
+                                    <div class="chitiet__product__option__item uk-padding-small uk-text-center">
+                                        <div class="chitiet__product__option__item__txt line-clamp-1"><?= $v['txt'] ?></div>
+                                        <div class="chitiet__product__option__item__price"><?= $v['price'] ?></div>
+                                    </div>
+                                </label>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <div class="uk-grid-10 uk-child-width-1-2@m uk-grid-match" uk-grid>
+                        <div class="uk-width-1-1">
+                            <a href="" class="chitiet__product__buyBtn chitiet__product__buyBtn--buy uk-button uk-button-default uk-button-large uk-width-1-1 uk-text-center">
+                                <div class="uk-width-1-1">
+                                    <div class="chitiet__product__buyBtn__txt1">ĐẶT MUA NGAY</div>
+                                    <small class="chitiet__product__buyBtn__txt2">Nhanh chóng, thuận tiện</small>
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="" class="chitiet__product__buyBtn chitiet__product__buyBtn--pay uk-button uk-button-default uk-button-large uk-width-1-1 uk-text-center">
+                                <div class="uk-width-1-1">
+                                    <div class="chitiet__product__buyBtn__txt1">MUA TRẢ GÓP</div>
+                                    <small class="chitiet__product__buyBtn__txt2">Thủ tục đơn giản</small>
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="" class="chitiet__product__buyBtn chitiet__product__buyBtn--cart uk-button uk-button-default uk-button-large uk-width-1-1 uk-text-center">
+                                <div class="uk-width-1-1">
+                                    <div class="chitiet__product__buyBtn__txt1">CHO VÀO GIỎ</div>
+                                    <small class="chitiet__product__buyBtn__txt2">Mua tiếp sản phẩm khác</small>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-1-4@m">
+                    <div class="uk-margin">
+                        <h6 class="uk-h6">Hiện đang có tại showroom:</h6>
+                        <ul class="uk-list uk-list-circle">
+                            <li>49 Thái Hà – P. Trung Liệt – Q. Đống Đa – Hà Nội</li>
+                            <li>151 Lê Thanh Nghị - Hai Bà Trưng - Hà Nội</li>
+                            <li>158-160 Lý Thường Kiệt – P.14 – Q. 10 – TP HCM</li>
+                            <li>84T/14 Trần Đình Xu – P. Cô Giang – Q. 1 – TP HCM</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
